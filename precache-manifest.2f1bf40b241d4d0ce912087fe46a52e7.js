69be1222a0fa976b09d08d5d87b98db88cde47b2
HieuL <hieul@now.vn>
2019-07-26 18:22:49 +0700

    Updates

---
+++ precache-manifest.2f1bf40b241d4d0ce912087fe46a52e7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b77a4bf4aa825ccc4b6ea3a5cf472d0",
+    "url": "/Hakathon/index.html"
+  },
+  {
+    "revision": "7ee74d89712347df6355",
+    "url": "/Hakathon/static/css/2.a317ec87.chunk.css"
+  },
+  {
+    "revision": "d90b63590965d0ec0c4c",
+    "url": "/Hakathon/static/css/main.ef008ee0.chunk.css"
+  },
+  {
+    "revision": "7ee74d89712347df6355",
+    "url": "/Hakathon/static/js/2.60f94cf8.chunk.js"
+  },
+  {
+    "revision": "d90b63590965d0ec0c4c",
+    "url": "/Hakathon/static/js/main.ec41014c.chunk.js"
+  },
+  {
+    "revision": "d1b79b9e211ab6a880b7",
+    "url": "/Hakathon/static/js/runtime~main.2417bb36.js"
+  },
+  {
+    "revision": "03e91f122aa5fd425abbe23c85546eb0",
+    "url": "/Hakathon/static/media/Linearicons-Free.03e91f12.woff2"
+  },
+  {
+    "revision": "2f3e9f80fff7d699dd3de6904d7d1647",
+    "url": "/Hakathon/static/media/Linearicons-Free.2f3e9f80.ttf"
+  },
+  {
+    "revision": "65060723fe964f85afa0a82d0bb78cf9",
+    "url": "/Hakathon/static/media/Linearicons-Free.65060723.woff"
+  },
+  {
+    "revision": "b9b7f23cb61b1f503e1249b63d980448",
+    "url": "/Hakathon/static/media/Linearicons-Free.b9b7f23c.eot"
+  },
+  {
+    "revision": "ec161a74448ccf2c18beefe8cca2b44a",
+    "url": "/Hakathon/static/media/Linearicons-Free.ec161a74.svg"
+  },
+  {
+    "revision": "bf3e70961027af61cec800afec28ff8b",
+    "url": "/Hakathon/static/media/banner.bf3e7096.jpg"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/Hakathon/static/media/fontawesome-webfont.1dc35d25.ttf"
+  },
+  {
+    "revision": "24c601e721ebd8279d38e2cfa0d01bc6",
+    "url": "/Hakathon/static/media/fontawesome-webfont.24c601e7.svg"
+  },
+  {
+    "revision": "25a32416abee198dd821b0b17a198a8f",
+    "url": "/Hakathon/static/media/fontawesome-webfont.25a32416.eot"
+  },
+  {
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
+    "url": "/Hakathon/static/media/fontawesome-webfont.c8ddf1e5.woff"
+  },
+  {
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "/Hakathon/static/media/fontawesome-webfont.e6cf7c6e.woff2"
+  },
+  {
+    "revision": "fb5fad3aa0a16fb2575f21fa6657b93f",
+    "url": "/Hakathon/static/media/home-banner.fb5fad3a.jpg"
+  },
+  {
+    "revision": "c1f25fd19da16b477fa68dccd31ff8d1",
+    "url": "/Hakathon/static/media/subscribe-bg.c1f25fd1.jpg"
+  }
+]);
